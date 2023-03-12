26861141bf1aa79dbc0b0cc3503ab88e13679e0c
ImAAAABattery <127628460+ImAAAABattery@users.noreply.github.com>
2023-03-12 23:36:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ var timeDiff = easterBreakStart.getTime() - today.getTime(); // räknar ut antal
 var daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // omvandlar millisekunderna till dagar och avrundar uppåt
 
 var weekdaysLeft = 0;
+var excludedDates = [
+  new Date(2023, 4, 6), // datumet för ledighet 1
+  new Date(2023, 4, 7) // datumet för ledighet 2
+];
 var currentDate = today;
 while (currentDate <= easterBreakStart) {
   var dayOfWeek = currentDate.getDay();
-  if (dayOfWeek !== 0 && dayOfWeek !== 6) {
+  var isExcludedDate = excludedDates.some(function(excludedDate) {
+    return excludedDate.getTime() === currentDate.getTime();
+  });
+  if (dayOfWeek !== 0 && dayOfWeek !== 6 && !isExcludedDate) {
     weekdaysLeft++;
   }
   currentDate.setDate(currentDate.getDate() + 1);
