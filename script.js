8f067b4d83aefacf4ee4d9e6a2e1c29b9417fb14
ImAAAABattery <127628460+ImAAAABattery@users.noreply.github.com>
2023-03-12 22:19:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var today = new Date(); // skapar ett datumobjekt för dagens datum
-var summerBreakStart = new Date(2023, 5, 15); // skapar ett datumobjekt för sommarlovets startdatum (15 juni 2023)
+var easterBreakStart = new Date(2023, 4, 11); // skapar ett datumobjekt för påsklovets startdatum (11 maj 2023)
 
-var timeDiff = summerBreakStart.getTime() - today.getTime(); // räknar ut antalet millisekunder mellan datumen
+var timeDiff = easterBreakStart.getTime() - today.getTime(); // räknar ut antalet millisekunder mellan datumen
 var daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // omvandlar millisekunderna till dagar och avrundar uppåt
 
+var weekdaysLeft = 0;
+var currentDate = today;
+while (currentDate <= easterBreakStart) {
+  var dayOfWeek = currentDate.getDay();
+  if (dayOfWeek !== 0 && dayOfWeek !== 6) {
+    weekdaysLeft++;
+  }
+  currentDate.setDate(currentDate.getDate() + 1);
+}
+
 document.getElementById("daysLeft").innerHTML = daysLeft; // visar antalet dagar kvar på webbsidan
+document.getElementById("schooldaysLeft").innerHTML = weekdaysLeft; // visar antalet vardagar kvar på webbsidan
